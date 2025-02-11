51feb3f7809fc96ce47231dfecf882ad288b8b3b
udaybm <mhatre.uday@gmail.com>
2025-02-11 15:03:21 +0530

    updated get andupdate api;s and localhost

--- blynkproxyapp.py
+++ blynkproxyapp.py
@@ -7,15 +7,17 @@ BLYNK_BASE_URL = "https://blynk.cloud/external/api"
 
 @app.route('/blynk/<token>/pin/<pin>', methods=['GET'])
 def get_blynk_value(token, pin):
-    url = f"{BLYNK_BASE_URL}/get?token={token}&pin={pin}"
+    #url = f"{BLYNK_BASE_URL}/get?token={token}&pin={pin}"
+    url=f"https://192.168.1.103:9443/get?token={token}&pin={pin}"
     response = requests.get(url)
     return jsonify(response.json())
 
 @app.route('/blynk/<token>/set/<pin>/<value>', methods=['GET'])
 def set_blynk_value(token, pin, value):
-    url = f"{BLYNK_BASE_URL}/update?token={token}&{pin}={value}"
+    #url = f"{BLYNK_BASE_URL}/update?token={token}&{pin}={value}"
+    url=f"https://192.168.1.103:9443/n4uICWW8guhAulM_Rwe2bG8hjcP3TzMB/update/V1?value=1"
     response = requests.get(url)
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True)
